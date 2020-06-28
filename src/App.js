60c781422918ea038d5593c9dc7f7dda1578029c
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-28 13:24:38 +0100

    add loading icon when fetching countries

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Search from "./components/Search";
 import CountriesList from "./components/CountriesList";
+import "./style/App.scss";
 
 function App() {
   return (
--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,10 +1,35 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./../style/CountriesList.scss";
+import Axios from "axios";
+
+const API_URL = "https://restcountries.eu/rest/v2/";
+const nCountriesToGet = 20;
 
 function CountriesList(props) {
+  const [countries, setCountries] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await Axios.get(`${API_URL}all`);
+      setCountries(response.data);
+      setIsLoading(false);
+    }
+    getData();
+  });
+
   return (
     <main className='CountriesList'>
-      <h2>Countries:</h2>
+      {isLoading ? (
+        <div className='CountriesList__loading'>
+          <i class='CountriesList__loading-icon fas fa-globe-americas'></i>
+          <span className='CountriesList__loading-text'>
+            Loading countries...
+          </span>
+        </div>
+      ) : (
+        <h2> NOT LOADING</h2>
+      )}
     </main>
   );
 }
