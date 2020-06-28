fafaaea366d9af678d66dcc5ba2cd073b7afe2c4
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-28 12:19:42 +0100

    add dark mode icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,9 @@ function Navbar(props) {
   return (
     <nav class='Navbar'>
       <h1 className='main-heading'>Where in the world?</h1>
-      <div className='color-mode'>Dark Mode</div>
+      <div className='color-mode'>
+        <i class='fas fa-moon'></i> Dark Mode
+      </div>
     </nav>
   );
 }
