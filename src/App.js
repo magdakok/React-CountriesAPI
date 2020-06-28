f54cd4ca13b4ae6df51bc9cbdaf4a5a73d5231b6
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-28 16:16:54 +0100

    install and initially implement React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Search from "./components/Search";
 import CountriesList from "./components/CountriesList";
+import CountryPage from "./components/CountryPage";
 import "./style/App.scss";
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
-      <Search />
-      <CountriesList />
+      <Switch>
+        <Route exact path='/' component={CountriesList} />
+        <Route exact path='/:country' component={CountryPage} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./style/base.scss";
 import "./style/typography.scss";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
