834cd533895e054f57282b64dad7e7686aa51e9e
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-28 21:25:00 +0100

    style CountriesList for bigger screen

--- src/components/CountryPreview.js
+++ src/components/CountryPreview.js
@@ -14,26 +14,28 @@ function CountryPreview(props) {
         src={props.flag}
         alt={`Flag of ${props.country}`}
       />
-      <div className='CountryPreview__info'>
-        <h2 className='CountryPreview__heading-country'>{props.country}</h2>
-        {props.population && (
-          <div className='CountryPreview__row'>
-            <h3 className='CountryPreview__heading-minor'>Population:</h3>
-            {populationWithComas}
-          </div>
-        )}
-        {props.region && (
-          <div className='CountryPreview__row'>
-            <h3 className='CountryPreview__heading-minor'>Region:</h3>
-            {props.region}
-          </div>
-        )}
-        {props.capital && (
-          <div className='CountryPreview__row'>
-            <h3 className='CountryPreview__heading-minor'>Capital:</h3>
-            {props.capital}
-          </div>
-        )}
+      <div className='CountryPreview__box'>
+        <div className='CountryPreview__info'>
+          <h2 className='CountryPreview__heading-country'>{props.country}</h2>
+          {props.population && (
+            <div className='CountryPreview__row'>
+              <h3 className='CountryPreview__heading-minor'>Population:</h3>
+              {populationWithComas}
+            </div>
+          )}
+          {props.region && (
+            <div className='CountryPreview__row'>
+              <h3 className='CountryPreview__heading-minor'>Region:</h3>
+              {props.region}
+            </div>
+          )}
+          {props.capital && (
+            <div className='CountryPreview__row'>
+              <h3 className='CountryPreview__heading-minor'>Capital:</h3>
+              {props.capital}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
