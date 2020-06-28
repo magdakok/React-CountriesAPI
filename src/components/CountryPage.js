48d012cb080cc23abbd9db1998920bb5f695e758
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-28 19:00:56 +0100

    add style and HTML structure to CountryPage component

--- src/components/CountryPage.js
+++ src/components/CountryPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./../style/CountryPage.scss";
 
 function CountryPage(props) {
   const {
     flag,
     name,
+    nativeName,
     population,
     region,
     subregion,
@@ -11,13 +13,44 @@ function CountryPage(props) {
     topLevelDomain,
     currencies,
     languages,
+    borders,
   } = props.country;
+
+  // const info = [
+  //   { "Native Name": nativeName },
+  //   { Population: population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") },
+  //   { Region: region },
+  //   { "Sub Region": subregion },
+  //   { Capital: capital },
+  //   { "Top Level Domain": topLevelDomain.join(", ") },
+  //   {
+  //     Currencies:
+  //       currencies.length > 1
+  //         ? "a lot"
+  //         : `${currencies[0].code}: ${currencies[0].name} (${currencies[0].symbol})`,
+  //   },
+  //   { Languages: languages },
+  //   { Borders: borders },
+  // ];
+  // console.log(info);
+
+  //  TO FUTURE MAGDA: make helper function that returns nice array with formatted data to strings!
   return (
     <div className='CountryPage'>
-      <button>
-        <i className='fas fa-long-arrow-alt-left'></i> BACK
+      <button className='CountryPage__backButton'>
+        <i className='fas fa-long-arrow-alt-left'></i> Back
       </button>{" "}
-      {(name, population)}
+      <div className='CountryPage__container'>
+        <div className='CountryPage__flagBox'>
+          <img src={flag} alt={`Flag of ${name}`} />
+        </div>
+        <div className='CountryPage__infoBox'>
+          <h2 className='CountryPage__heading-country'>{name}</h2>
+          <ul className='CountryPage__info'>
+            {/*here render all info items*/}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
