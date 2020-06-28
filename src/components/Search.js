07a07c65d1af175ee03434d873e68f7659abe1e4
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-28 12:17:50 +0100

    add search icon to the input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import "./../style/Search.scss";
 
+window.FontAwesomeConfig = {
+  searchPseudoElements: true,
+};
+
 function Search(props) {
   return (
     <div className='Search'>
-      <form className='Search__form'>
-        <input
-          className='Search__input'
-          type='text'
-          placeholder='Search for a country...'
-        />
-      </form>
+      <div className='Search__container'>
+        <form className='Search__form'>
+          <input
+            className='Search__input'
+            type='text'
+            placeholder='Search for a country...'
+          />
+        </form>
+      </div>
     </div>
   );
 }
