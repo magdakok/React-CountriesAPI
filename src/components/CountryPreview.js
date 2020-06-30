41ecba2d980e7367979112e011f9b70effe5a96c
magdakok <61601293+magdakok@users.noreply.github.com>
2020-06-30 10:20:03 +0100

    add and style hyperlink to all CountryPreview cards

--- src/components/CountryPreview.js
+++ src/components/CountryPreview.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
 import "./../style/CountryPreview.scss";
 
 function CountryPreview(props) {
@@ -8,36 +8,38 @@ function CountryPreview(props) {
     .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
   return (
-    <div className='CountryPreview'>
-      <img
-        className='CountryPreview__flag'
-        src={props.flag}
-        alt={`Flag of ${props.country}`}
-      />
-      <div className='CountryPreview__box'>
-        <div className='CountryPreview__info'>
-          <h2 className='CountryPreview__heading-country'>{props.country}</h2>
-          {props.population > 0 && (
-            <div className='CountryPreview__row'>
-              <h3 className='CountryPreview__heading-minor'>Population:</h3>
-              {populationWithComas}
-            </div>
-          )}
-          {props.region && (
-            <div className='CountryPreview__row'>
-              <h3 className='CountryPreview__heading-minor'>Region:</h3>
-              {props.region}
-            </div>
-          )}
-          {props.capital && (
-            <div className='CountryPreview__row'>
-              <h3 className='CountryPreview__heading-minor'>Capital:</h3>
-              {props.capital}
-            </div>
-          )}
+    <Link exact to={`${props.id}`}>
+      <div className='CountryPreview'>
+        <img
+          className='CountryPreview__flag'
+          src={props.flag}
+          alt={`Flag of ${props.country}`}
+        />
+        <div className='CountryPreview__box'>
+          <div className='CountryPreview__info'>
+            <h2 className='CountryPreview__heading-country'>{props.country}</h2>
+            {props.population > 0 && (
+              <div className='CountryPreview__row'>
+                <h3 className='CountryPreview__heading-minor'>Population:</h3>
+                {populationWithComas}
+              </div>
+            )}
+            {props.region && (
+              <div className='CountryPreview__row'>
+                <h3 className='CountryPreview__heading-minor'>Region:</h3>
+                {props.region}
+              </div>
+            )}
+            {props.capital && (
+              <div className='CountryPreview__row'>
+                <h3 className='CountryPreview__heading-minor'>Capital:</h3>
+                {props.capital}
+              </div>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
