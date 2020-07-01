3faa6647923a96c7649e3a7a7913aae274f7462e
magdakok <61601293+magdakok@users.noreply.github.com>
2020-07-01 18:26:56 +0100

    Revert "use InfiniteScroll component to render filtered countries"
    This reverts commit 9aa5837d0f0731c35415f9e272ca25fe0836ecec.

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,43 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import CountryPreview from "./CountryPreview";
-import Loading from "./Loading";
 import "./../style/CountriesList.scss";
-import InfiniteScroll from "react-infinite-scroll-component";
+
+const nCountriesToGet = 40;
 
 function CountriesList(props) {
   const { countries } = props;
-  const [displayed, setDisplayed] = useState(new Array(24).fill(null));
-
-  const fetchMoreData = () => {
-    setTimeout(() => {
-      setDisplayed(displayed.concat(Array.from({ length: 24 })));
-      //  setDisplayed(displayed.concat(Array.from({ length: 24 })));
-    }, 1500);
-  };
-
   return (
     <main className='CountriesList'>
-      {!countries.length ? (
-        <Loading />
-      ) : (
-        <InfiniteScroll
-          dataLength={displayed.length}
-          next={fetchMoreData}
-          hasMore={true}
-          loader={<Loading />}>
-          {displayed.map((i, index) => (
+      {countries.map((c, i) => {
+        if (i < nCountriesToGet) {
+          return (
             <CountryPreview
-              country={countries[index].name}
-              flag={countries[index].flag}
-              population={countries[index].population}
-              region={countries[index].subregion}
-              capital={countries[index].capital}
-              key={countries[index].alpha3Code}
-              id={countries[index].alpha3Code}
+              country={c.name}
+              flag={c.flag}
+              population={c.population}
+              region={c.subregion}
+              capital={c.capital}
+              key={c.alpha3Code}
+              id={c.alpha3Code}
             />
-          ))}
-        </InfiniteScroll>
-      )}
+          );
+        } else {
+          return;
+        }
+      })}
     </main>
   );
 }
