a472d25ec94576347015b024c18cdd3852081d4f
magdakok <61601293+magdakok@users.noreply.github.com>
2020-07-07 21:15:53 +0100

    Updates

---
+++ precache-manifest.8eea03c84c70d491fb40ddcf65979ca0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a4e876ce6c614612d1c0400d63b03f",
+    "url": "/React-CountriesAPI/index.html"
+  },
+  {
+    "revision": "45296b0ad1e70bf4e640",
+    "url": "/React-CountriesAPI/static/css/main.0bbf4e8a.chunk.css"
+  },
+  {
+    "revision": "0cde7e2516e4bc32d8e5",
+    "url": "/React-CountriesAPI/static/js/2.49fd6c7a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-CountriesAPI/static/js/2.49fd6c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45296b0ad1e70bf4e640",
+    "url": "/React-CountriesAPI/static/js/main.e5563c04.chunk.js"
+  },
+  {
+    "revision": "6ab4e6171c37d8283314",
+    "url": "/React-CountriesAPI/static/js/runtime-main.280ccb44.js"
+  }
+]);
