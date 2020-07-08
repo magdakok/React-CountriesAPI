6d426726340891459c1c4555c7b75e8ef188148b
magdakok <61601293+magdakok@users.noreply.github.com>
2020-07-08 15:00:40 +0100

    use local storage to keep info about theme

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,10 +1,14 @@
 import React, { createContext, useState } from "react";
-
 export const ThemeContext = createContext();
 
 export function ThemeProvider(props) {
-  const [isDarkMode, setThemeMode] = useState(false);
-  const toggleTheme = () => setThemeMode(!isDarkMode);
+  const [isDarkMode, setThemeMode] = useState(
+    JSON.parse(window.localStorage.getItem("theme") || false)
+  );
+  const toggleTheme = () => {
+    window.localStorage.setItem("theme", JSON.stringify(!isDarkMode));
+    setThemeMode(!isDarkMode);
+  };
   return (
     <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
       {props.children}
