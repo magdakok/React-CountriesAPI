06c14e330f58669132334d8ad4d68aed4720f767
magdakok <61601293+magdakok@users.noreply.github.com>
2020-07-22 21:21:44 +0100

    add link to h1 redirecting to the main page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "./../contexts/ThemeContext";
+import { Link } from "react-router-dom";
 import "./../style/Navbar.scss";
 
 function Navbar(props) {
   const { isDarkMode, toggleTheme } = useContext(ThemeContext);
   return (
     <nav className={isDarkMode ? "Navbar Navbar--dark" : "Navbar"}>
-      <h1 className='main-heading'>Where in the world?</h1>
+      <Link to='/'>
+        <h1 className='main-heading'>Where in the world?</h1>
+      </Link>
       <div
         className={isDarkMode ? "color-mode color-mode--dark" : "color-mode"}
         onClick={toggleTheme}>
